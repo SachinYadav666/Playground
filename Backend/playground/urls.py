da8a224777b54f10af2b92eaad2c7bb9bd1e3212
Sachin Yadav <sachintmg666@gmail.com>
2025-06-21 14:53:43 +0530

    change urls.py

--- Backend/playground/urls.py
+++ Backend/playground/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from playground.views import Chat
 
 urlpatterns = [
-    path('chat/', Chat.as_view(), name='chat'),  
+    path('chat/', Chat.as_view()),  
 ]
--- Backend/setup/urls.py
+++ Backend/setup/urls.py
@@ -1,7 +1,10 @@
 from django.contrib import admin
 from django.urls import include, path
 
+from django.http import JsonResponse
+
 urlpatterns = [
-    path("", include("playground.urls")),
-    path('admin/', admin.site.urls)
+    path("", lambda request: JsonResponse({"status": "Backend is live"})),
+    path("playground/", include("playground.urls")),
+    path("admin/", admin.site.urls),
 ]
